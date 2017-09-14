6b08298c8ff5383a666f4017627e1dbfa007045e
phuthanh19951 <phuthanh19951@gmail.com>
2017-09-15 00:30:43 +0700

    Copy Models from FoodyProject

---
+++ app/Models/Food.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+  protected $table = "tbl_food";
+
+  public $timestamps = true;
+
+  protected $fillable = [
+      "name" ,
+      "price",
+      "post_id",
+      "category_food_id",
+      "url_image"
+  ];
+
+ public function Post(){
+   return $this->belongsTo('App\Models\Post');
+ }
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+  //
+  use Notifiable;
+
+  use SoftDeletes;
+
+  protected $table = "tbl_users";
+
+  public $timestamps = true;
+  protected $softDelete = true; // Bật tính năng.
+  protected $dates = ['deleted_at']; // Thuộc tính thực hiện softdeleted.
+
+  protected $fillable = [
+      'email',
+      'password',
+      'fullname',
+      'gender',
+      'address',
+      'phone',
+      'birthday',
+      'url_image',
+  ];
+
+  protected $hidden = ['remember_token','password'];
+
+  public function Comments()
+  {
+    return $this->hasMany('App\Models\Comment');
+  }
+
+  public function Comment()
+  {
+    return $this->hasOne('App\Models\Comment');
+  }
+
+  public function Posts()
+  {
+    return $this->hasMany('App\Models\Post','insert_id','id');
+  }
+
+}
