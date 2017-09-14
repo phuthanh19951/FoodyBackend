1b0eae76a702d84d1d4f9f64e60e6593b2a5a730
phuthanh19951 <phuthanh19951@gmail.com>
2017-09-15 00:34:30 +0700

    Commit api route.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::group ( [
+    'namespace'  => '\API\V1',
+    'prefix'     => 'v1',
+    'middleware' => 'cors'
+], function () {
+
+//  Route::resource('post','PostController');
+
+  Route::get('province','CommonController@getProvince');
+  Route::get('category','CommonController@getCategoryPost');
+  Route::get('district/{provinceId}','CommonController@getDistrictByProvinceId');
+
+  //Route for searching post
+  Route::get('searchname','PostController@searchByName');
+  Route::get('getimages/{postid}/{typeid?}','ImagesController@getImages');
+
+  // Route for Post Resource
+  Route::resource('posts','PostController');
+  // Route for Subcomment Resource
+  Route::resource('subcomments','SubCommentController');
+
 });
