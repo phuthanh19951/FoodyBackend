137b19ef186da642ccbbab7709799f1449c16675
phuthanh19951 <phuthanh19951@gmail.com>
2017-10-13 14:40:49 +0700

    CORS update(1)

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -17,6 +17,7 @@ class CORS
     {
       return $next($request)
           ->header('Access-Control-Allow-Origin', '*')
-          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+          ->header('Access-Control-Allow-Headers','Content-Type, Authorization, X-Requested-With');
     }
 }
