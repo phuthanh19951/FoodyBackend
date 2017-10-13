9753695039ff056de22a961f32ae6be0205ec9ff
phuthanh19951 <phuthanh19951@gmail.com>
2017-10-13 14:39:22 +0700

    Controller update(1)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,41 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Carbon\Carbon;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function formatNumber($number){
+    $number = intval($number);
+    if($number < 10){
+      $number = '0' . $number;
+    }
+    return $number;
+  }
+
+  public function setDefaultValue($object,$isAddNew){
+    // If isAddNew parameter is true .
+    if($isAddNew) {
+//        $object->insert_id = Auth::guard('admin')->user()->id;
+      $object->created_at = Carbon::now();
+    }
+    $object->updated_at = Carbon::now();
+  }
+
+  function cvf_convert_object_to_array($data) {
+
+    if (is_object($data)) {
+      $data = get_object_vars($data);
+    }
+
+    if (is_array($data)) {
+      return array_map(array($this,__FUNCTION__), $data);
+    }
+    else {
+      return $data;
+    }
+  }
+
 }
