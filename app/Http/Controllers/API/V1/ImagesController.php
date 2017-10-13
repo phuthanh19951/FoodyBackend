2b88d358817b22f6dd8890a450ecec045d4d9697
phuthanh19951 <phuthanh19951@gmail.com>
2017-10-13 14:33:54 +0700

    Image controller update(1)

--- app/Http/Controllers/API/V1/ImagesController.php
+++ app/Http/Controllers/API/V1/ImagesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use Illuminate\Auth\Access\Response;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Models\Image;
 
 class ImagesController extends Controller
 {
@@ -16,6 +18,8 @@ class ImagesController extends Controller
     public function index()
     {
         //
+      echo 2;
+      print_r($_FILES);
     }
 
     /**
@@ -36,7 +40,44 @@ class ImagesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $response = array(
+          'status' => 400,
+          'data'   => '',
+          'error'  => ''
+      );
+
+      // Check directory of post is exist.
+      $pathDir = $_POST['path'].$_POST['postId'];
+      if (!is_dir($pathDir)) {//hình menu
+        mkdir($pathDir);
+      }
+
+      if(isset($_FILES['images']) && !empty($_FILES['images'])){
+        if(isset($_POST['postId']) && is_numeric($_POST['postId'])){
+          for ($i = 0; $i < count($_FILES['images']['name']); $i++) {
+            $image = Image::create([
+                'name' => $_FILES['images']['name'][$i],
+                'type' => $_FILES['images']['type'][$i],
+                'size' => $_FILES['images']['size'][$i],
+                'post_id' => $_POST['postId'],
+                'category_image' => $_POST['typeImages'][$i],
+                'comment_id' => 0,
+                'insert_id' => isset($_POST['userId']) ? $_POST['userId'] : 1,
+                'url_image' => 'public/uploads/photo/' . $_POST['postId'] . '/' . $_FILES['images']['name'][$i]
+            ]);
+            // Check file exist or not .
+            if( !file_exists($pathDir.'/'.$_FILES['images']['name'][$i]) ){
+              move_uploaded_file($_FILES['images']['tmp_name'][$i],$pathDir.'/'.$_FILES['images']['name'][$i]);
+            }
+            if(count($image) > 0){
+              $response['status'] = 200;
+            }
+          }
+        }
+      }
+
+
+      return \Response::json($response);
     }
 
     /**
@@ -86,8 +127,10 @@ class ImagesController extends Controller
 
   public function getImages($postId,$typeImage = 0){
     $response = [
-        "imageList" => "",
-        "status"   => 1
+        "data" => array(
+            "imageList" => array()
+        ),
+        "status"   => 204
     ];
 
     if(isset($postId) && is_numeric($postId)){
@@ -95,20 +138,17 @@ class ImagesController extends Controller
       if(isset($typeImage) && is_numeric($typeImage)){
         if($typeImage != 0){
           $imageList = $imageList->where('category_image',$typeImage);
+          // Get image from tbl_image_comment table.
         }
       }
       $imageList = $imageList->get();
-      if(count($imageList) > 0){
-        $response['imageList'] = $imageList;
-      }else{
-        $response['status'] = 0;
+      if( count($imageList) > 0 ){
+        $response['data']['imageList'] = $imageList;
+        $response['status'] = 200;
       }
-    }else{
-      $response['status'] = 0;
     }
 
     return \Response::json($response);
-
   }
 
 }
